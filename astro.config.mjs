4b71cfe3dc7a3a7fc2914895e0370d49393802ec
IgamiLia <alejandrofuego27@gmail.com>
2026-06-30 16:41:13 -0300

    UPGRADE

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mattecocido.com',
   vite: {
     plugins: [tailwindcss()],
     resolve: {
